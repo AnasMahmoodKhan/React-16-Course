6871d4d52047ae5fb8e593097876c6900fbea9e6
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-07 18:22:17 +0530

    Add Backdrop to order summary modal

--- burger_builder/src/components/UI/Modal/Modal.js
+++ burger_builder/src/components/UI/Modal/Modal.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Aux from "../../../hoc/Auxillary";
+import Backdrop from "../Backdrop/Backdrop";
 import "./Modal.css";
-const Modal = ({ children, show }) => {
+const Modal = ({ children, show, modalClose }) => {
   return (
-    <div
-      className="Modal"
-      style={{
-        transform: show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: show ? "1" : "0",
-      }}
-    >
-      {children}
-    </div>
+    <Aux>
+      <Backdrop show={show} clicked={modalClose} />
+      <div
+        className="Modal"
+        style={{
+          transform: show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: show ? "1" : "0",
+        }}
+      >
+        {children}
+      </div>
+    </Aux>
   );
 };
 
