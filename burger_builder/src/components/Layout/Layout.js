0326303a11d01a0343f5307a2a24d0c85e61d21e
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-07 21:19:59 +0530

    Add sideDrawer

--- burger_builder/src/components/Layout/Layout.js
+++ burger_builder/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Auxillary";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import "./Layout.css";
 
 const Layout = (props) => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className="Content">{props.children}</main>
   </Aux>
 );
