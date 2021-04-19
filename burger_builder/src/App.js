8dc18c92f2fad63f53df1cb3376876a45bdae742
AnasMahmoodKhan <77839562+AnasMahmoodKhan@users.noreply.github.com>
2021-04-19 21:02:11 +0530

    Added checkout and UI components

--- burger_builder/src/App.js
+++ burger_builder/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/" />
+          </Switch>
         </Layout>
       </div>
     );
